82783fab87ebd2469305b2375965ea3e5d7dcbcc
Denis1818 <permpdv@gmail.com>
2026-02-27 11:51:25 +0500

    Create TaskSolvers.h

---
+++ TaskSolvers.h
@@ -0,0 +1,19 @@
+#ifndef TASKSOLVERS_H
+#define TASKSOLVERS_H
+
+/// <summary>
+/// Демонстрирует решение задачи о музыкальных произведениях с использованием класса MySet.
+/// </summary>
+void SolveTask1();
+
+/// <summary>
+/// Читает данные из файла "students.txt" и выводит школы со средним баллом выше среднего по району.
+/// </summary>
+void SolveTask2();
+
+/// <summary>
+/// Демонстрирует работу классов Pistol и Automatic.
+/// </summary>
+void SolveTask3();
+
+#endif // TASKSOLVERS_H
